4f5c12cd383a344bf485d496bcf91fcd57039e72
SyedAhmadShaan <SyedAhmadShaan@users.noreply.github.com>
2024-04-26 19:42:01 +0500

    Question no. 18 in progress

--- learn-typescript/Solving-45-TypeScript-Questions/index.ts
+++ learn-typescript/Solving-45-TypeScript-Questions/index.ts
@@ -110,3 +110,23 @@ console.log(`${Guest[1]}, Please join us for dinner this weekend`);
 console.log(`${Guest[2]}, Please join us for dinner this weekend`);
 
 // 17. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
+// Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
+console.log(
+  "My new dinner table won’t arrive in time for the dinner, I can invite only two people for dinner"
+);
+// • Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+while (Guest.length > 2) {
+  console.log(`${Guest.pop()}, Sorry, I can't invite you to dinner`);
+  console.log(`${Guest.pop()}, Sorry, I can't invite you to dinner`);
+  console.log(Guest);
+  // • Print a message to each of the two people still on your list, letting them know they’re still invited.
+  console.log(`${Guest[0]}, You are still invited to dinner`);
+  console.log(`${Guest[1]}, You are still invited to dinner`);
+}
+
+// • Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+Guest.splice(0, 2);
+console.log(Guest);
+
+// 18. Seeing the World: Think of at least five places in the world you’d like to visit.
+// • Store the locations in a array. Make sure the array is not in alphabetical order.
