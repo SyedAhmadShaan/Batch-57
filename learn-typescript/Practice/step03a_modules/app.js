67df3649165cb392ef4d73b1c85d238c7f20f51c
SyedAhmadShaan <SyedAhmadShaan@users.noreply.github.com>
2024-05-25 19:33:21 +0500

    modules

--- learn-typescript/Practice/step03a_modules/app.js
+++ learn-typescript/Practice/step03a_modules/app.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const first_1 = __importDefault(require("./first"));
+const first_1 = __importDefault(require("./first")); //import
 console.log("a", first_1.default);
 const Second_1 = require("./Second");
 console.log(Second_1.sum);
