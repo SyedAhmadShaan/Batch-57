61d749a80edafe27294f8d374c22bf128660b56f
SyedAhmadShaan <SyedAhmadShaan@users.noreply.github.com>
2024-06-26 23:49:51 +0500

    word-counter-tool-cli

---
+++ learn-typescript/Node Projects/Word-Counter/dist/index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function main() {
+    try {
+        const countWords = await inquirer.prompt([
+            {
+                name: "enterText",
+                type: "input",
+                message: "Enter or paste your text:",
+            },
+        ]);
+        const words = countWords.enterText;
+        console.log("Received text:", words);
+        console.log(`The length of words is ${words.length} characters`);
+    }
+    catch (error) {
+        console.log("An error aoccured:", error);
+    }
+}
+main();
